d3bd543e5bc81dc2161cfba8cc83c8887756e7c7
Nicolas Gonzalez <38365041+porwaltz@users.noreply.github.com>
2022-10-03 12:14:31 -0300

    como investigar full

--- educacion-virtual/src/pages/Documents/ComoInvestigar.js
+++ educacion-virtual/src/pages/Documents/ComoInvestigar.js
@@ -45,6 +45,14 @@ export default function ComoInvestigar(){
                     para todos aquellos estudiantes que quieran comenzar una investigación en derecho.
                 </p>
             </section>
+            <section >
+                <hr/>
+                <p class="aclaration"><sup>1</sup> Plan de estudios carrera Abogacía, EXP-UBA: 45.501/2018, en <br/>
+                    <a href="http://www.derecho.uba.ar/academica/carreras_grado/pdf/plan-de-estudios-abogacia.pdf" target="blank">
+                        &nbsp;http://www.derecho.uba.ar/academica/carreras_grado/pdf/plan-de-estudios-abogacia.pdf
+                    </a> 
+                </p>
+            </section>
             <section>
                 <p class="underline subtitle"><span class="bold">Desarrollo: </span><span class="italic">Investigación para estudiantes</span></p>
                 <p>
@@ -69,7 +77,7 @@ export default function ComoInvestigar(){
             <section>
                 <p class="underline subtitle">2) NUEVOS RECURSOS DE EDUCACIÓN VIRTUAL </p>
                 <p>
-                    <span class="bold">&#8226;</span> 2.a) <span class="italic">Textos hipervinculados</span> (enseñar desde el ejemplo):
+                    <span>&#8226;</span> 2.a) <span class="italic">Textos hipervinculados</span> (enseñar desde el ejemplo):
                     <br/> 
                     Proponemos una herramienta de enseñanza virtual a través de un ejemplo concreto a fin de brindar, de forma didáctica, 
                     aquellos aspectos esenciales para iniciar una investigación en derecho.
@@ -80,6 +88,68 @@ export default function ComoInvestigar(){
                     (videos, imágenes, textos, etc.). Todo ello conformará una suerte de “bitácora del investigador”, 
                     que contendrá la información y explicación de los primeros pasos que el estudiante debería dar 
                     para la redacción de un trabajo y/o proyecto de investigación.
+                </p>
+                <p>
+                    <span>&#8226;</span> 2.b) <span class="italic">Foro de borradores:</span>
+                    <br/> 
+                    Generar un espacio en el cual todos los usuarios puedan “subir” sus borradores de proyectos y trabajos de investigación, 
+                    para que los administradores hagan sugerencias que se encontrarán visibles en el foro, 
+                    a fin de que los usuarios puedan aprender de las observaciones e incluso hacer preguntas y comentarios.
+
+                </p>
+            </section>
+            <section >
+                <hr/>
+                <p class="aclaration">
+                    <sup>2</sup> Sin dejar de lado la posibilidad de expandir dichos recursos a la enseñanza de otros contenidos de interés.
+                </p>
+            </section>
+            <section>
+                <p class="underline subtitle"><span class="bold">Conclusión: </span><span class="italic">Recapitulaciones finales sobre la propuesta.</span></p>
+                <p>
+                    Si bien existen diversos recursos y posibilidades para investigar en la facultad, los estudiantes de grado sin conocimientos previos en la materia, 
+                    encuentran dificultades para conocer y acceder a ellos. Por dicha razón, valiéndonos de las nuevas tecnologías para la enseñanza, 
+                    consideramos que las propuestas desarrolladas pueden acercar aún más, la investigación a los alumnos de derecho.
+                </p>
+            </section>
+            <section>
+                <p class="underline subtitle"><span class="bold">Bibliografía </span></p>
+                <p>
+                    <span>&#8226;&emsp;</span>CENTRO DE DERECHO DE INVESTIGACIÓN AMBIENTAL - FACULTAD DE DERECHO, 
+                    UBA - “GUÍA METODOLÓGICA PARA DISEÑOS DE INVESTIGACIÓN” Cedaf. Facultad de Derecho, UBA. 
+                    Disponible al 01/08/22 en: 
+                    <a href="http://www.derecho.uba.ar/institucional/centro-derecho-ambiental/documentos/guia-metodologica-para-disenos-de-investigacion.pdf" target="blank">
+                        &nbsp;http://www.derecho.uba.ar/institucional/centro-derecho-ambiental/documentos/guia-metodologica-para-disenos-de-investigacion.pdf
+                    </a>
+                </p>
+                <p>
+                    <span>&#8226;&emsp;</span>DABOVE, Maria Isolina y ORLER, Jose. La promoción de la investigación científica en las Facultades de Derecho: 
+                    un abordaje crítico del modelo Académico Argentino.
+                </p>
+                <p>
+                    <span>&#8226;&emsp;</span>DERECHO AL DIA, “¿Cómo investigar en la Facultad de Derecho?”  Buenos Aires, Revista Derecho al Día, Año XX - 
+                    Edición 357 - 24 de junio de 2021. Disponible al 29/09/2022 en: 
+                    <a href="http://www.derecho.uba.ar/derechoaldia/notas/como-investigar-en-la-facultad-de-derecho/+8356" target="blank">
+                        &nbsp;http://www.derecho.uba.ar/derechoaldia/notas/como-investigar-en-la-facultad-de-derecho/+8356
+                    </a>
+                </p>
+                <p>
+                    <span>&#8226;&emsp;</span>NAVARRO, Federico. Escritura e inclusión en la universidad. Herramientas para docentes.
+                </p>
+                <p>
+                    <span>&#8226;&emsp;</span>LORA, Laura Noemí, “Nociones Básicas de Metodología de la Investigación Social”, 
+                    Guía didáctica para elaborar trabajos prácticos en el marco de los cursos de Metodología de la Investigación Social, 
+                    El Instituto de la Adopción y Problemas Actuales de Sociología Jurídica. 
+                    Buenos Aires: Facultad de Derecho de la Universidad de Buenos Aires, 2015.
+                </p>
+                <p>
+                    <span>&#8226;&emsp;</span>PERRENOUD, P. “La construcción del éxito y del fracaso escolar Hacia un análisis del éxito, 
+                    del fracaso y de las desigualdades como realidades construidas por el sistema escolar”  
+                    Editorial: FUNDACIÓN PADEIA EDICIONES MORATA – Madrid, 1991.
+                </p>
+                <p>
+                    <span>&#8226;&emsp;</span>YUNI, JOSE; URBANO CLAUDIO, 
+                    “Tecnicas para investigar: recursos metodológicos para la preparación de proyectos de investigación.
                 </p>
             </section>
         </article>
